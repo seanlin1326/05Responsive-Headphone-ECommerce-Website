3c9d1aa23035ecfba7587e5dbe82221164fcab2b
SeanLin <seanlin1326@gmail.com>
2024-11-27 11:36:37 +0800

    feat:初始環境

---
+++ practice/js/main-practice.js
@@ -0,0 +1,19 @@
+// nav scroll-on
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll= function(){
+    if(document.documentElement.scrollTop > 20)
+    {
+        nav.classList.add('scroll-on');
+    }
+    else
+    {
+        nav.classList.remove('scroll-on');
+    }
+}
+let navLink = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navLink.forEach(function(e){
+    e.addEventListener('click',function(){
+        navCollapse.classList.remove('show');
+    })
+})
